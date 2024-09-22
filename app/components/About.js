3a26be8ee8431f90dc7eec58394559b9aa32e02e
hodonnell <hodonnell@nymbus.com>
2024-09-22 12:02:11 -0400

    removed special chars

--- app/components/About.js
+++ app/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div id="about" className="h-[100dvh] grid bg-white p-4 sm:p-24">
       <div className="relative flex justify-center font-bold text-2xl mb-10">
-        <h1 className='text-4xl z-10 text-[rgb(75,69,149)]'>What We're About</h1>
+        <h1 className='text-4xl z-10 text-[rgb(75,69,149)]'>What We&apos;re About</h1>
         <div className='absolute bg-[#eaebff] h-12 w-72 top-4 right-[36dvw] blur-md z-2'></div>
       </div>
       <div className="grid grid-rows-2 gap-20 p-2 sm:p-20">
--- app/components/Testimonials.js
+++ app/components/Testimonials.js
@@ -14,15 +14,15 @@ export default function Testimonials() {
         <h2 className="text-4xl font-bold mb-24">What Our Users Are Saying</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20">
         <div className="bg-white border-2 shadow rounded-lg p-8">
-            <p className="text-gray-700 mb-4">"This app has been a game changer for me! I highly recommend it to anyone looking to streamline their workflow."</p>
+            <p className="text-gray-700 mb-4">&quot;This app has been a game changer for me! I highly recommend it to anyone looking to streamline their workflow.&quot;</p>
             <p className="text-gray-700 font-medium">- John Doe, CEO</p>
         </div>
         <div className="bg-white shadow rounded-lg p-8">
-            <p className="text-gray-700 mb-4">"I've tried a lot of different apps, but this one really stands out. It's so easy to use, and the features are exactly what I need."</p>
+            <p className="text-gray-700 mb-4">&quot;I&apos;ve tried a lot of different apps, but this one really stands out. It&apos;s so easy to use, and the features are exactly what I need.&quot;</p>
             <p className="text-gray-700 font-medium">- Jane Smith, Designer</p>
         </div>
         <div className="bg-white shadow rounded-lg p-8">
-            <p className="text-gray-700 mb-4">"I love how customizable this app is. I can really make it work for me, no matter what project I'm working on."</p>
+            <p className="text-gray-700 mb-4">&quot;I love how customizable this app is. I can really make it work for me, no matter what project I&apos;m working on.&quot;</p>
             <p className="text-gray-700 font-medium">- Bob Johnson, Developer</p>
         </div>
         </div>
